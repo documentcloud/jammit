65e6434ece6692899252407778e3c63a8381e77a
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-16 15:33:19 -0500

    added a rake test for the jammit helpers

---
+++ test/unit/test_jammit_helpers.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+require 'action_controller'
+require 'action_view'
+require 'action_view/test_case'
+require 'jammit/helper'
+
+class ActionController::Base
+  cattr_accessor :asset_host
+end
+
+class JammitHelpersTest < ActionView::TestCase
+  include ActionView::Helpers::AssetTagHelper
+  include Jammit::Helper
+
+  def test_include_stylesheets
+    assert include_stylesheets(:test) == File.read('fixtures/tags/css_includes.html')
+  end
+
+  def test_include_javascripts
+    assert include_javascripts(:test) == '<script src="/assets/test.js" type="text/javascript"></script>'
+  end
+
+  def test_include_templates
+    assert include_templates(:test) == '<script src="/assets/test.jst" type="text/javascript"></script>'
+  end
+
+end
