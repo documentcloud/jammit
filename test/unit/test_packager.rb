5da8a307ced35437814bfd5d03a95c5498d6eb9b
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-16 14:42:31 -0500

    added Jammit::Packager tests for JS and JST

--- test/unit/test_packager.rb
+++ test/unit/test_packager.rb
@@ -9,13 +9,23 @@ class PackagerTest < Test::Unit::TestCase
     assert urls == ['fixtures/src/test1.js', 'fixtures/src/test2.js']
   end
 
-  def test_packaging_a_css_package
+  def test_packaging_stylesheets
     css = Jammit.packager.pack_stylesheets(:test)
     assert css == File.read('fixtures/jammed/test.css')
     css = Jammit.packager.pack_stylesheets(:test, :datauri)
     assert css == File.read('fixtures/jammed/test-datauri.css')
     css = Jammit.packager.pack_stylesheets(:test, :mhtml, 'http://www.example.com')
     assert css == File.read('fixtures/jammed/test-mhtml.css')
+  end
+
+  def test_packaging_javascripts
+    js = Jammit.packager.pack_javascripts(:test)
+    assert js == File.read('fixtures/jammed/test.js')
+  end
+
+  def test_packaging_templates
+    jst = Jammit.packager.pack_templates(:test)
+    assert jst == File.read('fixtures/jammed/test.jst')
   end
 
 end
