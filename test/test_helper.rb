8b7ec7cb292aeed5ea6d76a608b51304e8469f9b
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-16 12:29:33 -0500

    fixing tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,10 @@ RAILS_ROOT = File.expand_path('test') unless defined? RAILS_ROOT
 # Mock out missing pieces.
 module ActionController
   class Base
-    def self.caches_page(*args)
+    def self.perform_caching
+      true
+    end
+    def self.after_filter(*args)
     end
   end
 end
