42da23841260b7998fd148d1991c9871b2d4e916
Michael Nutt <michael@nuttnet.net>
2010-08-02 15:33:26 -0400

    Rails3 router no longer yields a map

--- rails/routes.rb
+++ rails/routes.rb
@@ -1,6 +1,6 @@
 if defined?(Rails::Application)
   # Rails3 routes
-  Rails.application.routes.draw do |map|
+  Rails.application.routes.draw do
     match "/#{Jammit.package_path}/:package.:extension",
       :to => 'jammit#package', :as => :jammit
   end
