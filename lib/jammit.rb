da9758cdbce0a2c9d8e37d308d054540174cf7e1
Jeremy Ashkenas <jashkenas@gmail.com>
2010-11-08 08:47:11 -0500

    Jammit 0.5.4

--- lib/jammit.rb
+++ lib/jammit.rb
@@ -4,7 +4,7 @@ $LOAD_PATH.push File.expand_path(File.dirname(__FILE__))
 # to all of the configuration options.
 module Jammit
 
-  VERSION               = "0.5.3"
+  VERSION               = "0.5.4"
 
   ROOT                  = File.expand_path(File.dirname(__FILE__) + '/..')
 
