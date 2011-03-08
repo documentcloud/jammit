135103b380c1df5fe39ad9f7605e5b4022dca5dc
Christoffer Klang <christoffer.klang@gmail.com>
2011-03-08 23:41:15 +0100

    Add support for environment specific settings in configuration file

--- test/unit/test_configuration.rb
+++ test/unit/test_configuration.rb
@@ -62,4 +62,15 @@ class BrokenConfigurationTest < Test::Unit::TestCase
     assert packed == File.read('test/fixtures/jammed/jst_test.js')
   end
 
+  def test_environment_specific_configuration
+    Object.send(:remove_const, :RAILS_ENV)
+    Object.const_set :RAILS_ENV, "development"
+    Jammit.load_configuration('test/config/assets-environment.yml')
+
+    assert !Jammit.compress_assets # Should override with environment specific configuration
+    assert Jammit.gzip_assets # but keep the general configuration
+
+    Object.send(:remove_const, :RAILS_ENV)
+    Object.const_set :RAILS_ENV, "test"
+  end
 end
