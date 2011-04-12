13340a32b7c18c44c15756b886eec3c47a4673f4
Brandon Bloom <brandon@brandonbloom.name>
2011-04-11 17:55:34 -0700

    Extract compile_single_jst from compile_jst to make for easier monkey patching replacement of compilation logic

--- lib/jammit/compressor.rb
+++ lib/jammit/compressor.rb
@@ -99,14 +99,18 @@ module Jammit
       paths       = paths.grep(Jammit.template_extension_matcher).sort
       base_path   = find_base_path(paths)
       compiled    = paths.map do |path|
-        contents  = read_binary_file(path)
-        contents  = contents.gsub(/\r?\n/, "\\n").gsub("'", '\\\\\'')
         name      = template_name(path, base_path)
-        "#{namespace}['#{name}'] = #{Jammit.template_function}('#{contents}');"
+        "#{namespace}['#{name}'] = #{compile_single_jst(path)};"
       end
       compiler = Jammit.include_jst_script ? read_binary_file(DEFAULT_JST_SCRIPT) : '';
       setup_namespace = "#{namespace} = #{namespace} || {};"
       [JST_START, setup_namespace, compiler, compiled, JST_END].flatten.join("\n")
+    end
+
+    def compile_single_jst(path)
+      contents  = read_binary_file(path)
+      contents  = contents.gsub(/\r?\n/, "\\n").gsub("'", '\\\\\'')
+      "#{Jammit.template_function}('#{contents}')"
     end
 
 
