cdcec0a3d695935921d101deb707222c5587ccc3
dblock <dblock@dblock.org>
2011-05-10 10:45:19 -0400

    Added rewrite_relative_paths.

--- lib/jammit.rb
+++ lib/jammit.rb
@@ -52,7 +52,8 @@ module Jammit
                   :embed_assets, :package_assets, :compress_assets, :gzip_assets,
                   :package_path, :mhtml_enabled, :include_jst_script, :config_path,
                   :javascript_compressor, :compressor_options, :css_compressor_options,
-                  :template_extension, :template_extension_matcher, :allow_debugging
+                  :template_extension, :template_extension_matcher, :allow_debugging,
+                  :rewrite_relative_paths
     attr_accessor :compressors
   end
 
@@ -77,6 +78,7 @@ module Jammit
     @package_path           = conf[:package_path] || DEFAULT_PACKAGE_PATH
     @embed_assets           = conf[:embed_assets] || conf[:embed_images]
     @compress_assets        = !(conf[:compress_assets] == false)
+    @rewrite_relative_paths = !(conf[:rewrite_relative_paths] == false)
     @gzip_assets            = !(conf[:gzip_assets] == false)
     @allow_debugging        = !(conf[:allow_debugging] == false)
     @mhtml_enabled          = @embed_assets && @embed_assets != "datauri"
