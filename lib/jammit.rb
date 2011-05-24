431140c6b73dcf8d84e98258a81e2be7cae2c434
Jeremy Ashkenas <jashkenas@gmail.com>
2011-05-24 10:00:05 -0400

    safer check for RAILS_ENV, #177

--- lib/jammit.rb
+++ lib/jammit.rb
@@ -68,7 +68,7 @@ module Jammit
     conf = YAML.load(ERB.new(File.read(config_path)).result)
 
     # Optionally overwrite configuration based on the environment.
-    rails_env = defined?(Rails) ? Rails.env : RAILS_ENV
+    rails_env = defined?(Rails) ? Rails.env : ENV['RAILS_ENV']
     conf.merge! conf.delete rails_env if conf.has_key? rails_env
 
     @config_path            = config_path
