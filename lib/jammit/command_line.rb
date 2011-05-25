f4386194665fcd7ae9e6af4c44973350922fbf46
Jeremy Ashkenas <jashkenas@gmail.com>
2011-05-25 16:03:21 -0400

    Jammit 0.6.1

--- lib/jammit/command_line.rb
+++ lib/jammit/command_line.rb
@@ -63,7 +63,7 @@ Options:
         opts.on('-f', '--force', 'force a rebuild of all assets') do |force|
           @options[:force] = force
         end
-        opts.on('-p', '--packages PACKAGE_NAMES', 'comma-separated list of packages (ex: "core,embed", default: all packages)') do |package_names|
+        opts.on('-p', '--packages LIST', 'list of packages to build (ex: "core,ui", default: all)') do |package_names|
           @options[:package_names] = package_names.split(/,\s*/).map {|n| n.to_sym }
         end
         opts.on_tail('-v', '--version', 'display Jammit version') do
