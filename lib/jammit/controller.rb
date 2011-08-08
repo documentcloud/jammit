8b63ee1d5c707086fe9f7d5ef17766bcd858acfe
Rebecca Miller-Webster <rebecca@graciouseloise.com>
2011-08-08 14:23:17 -0400

    check whether Rails responds to env in controller.rb

--- lib/jammit/controller.rb
+++ lib/jammit/controller.rb
@@ -90,7 +90,7 @@ end
 # Make the Jammit::Controller available to Rails as a top-level controller.
 ::JammitController = Jammit::Controller
 
-if defined?(::Rails) && ::Rails.env.development?
+if defined?(::Rails) && ::Rails.respond_to?(:env) && ::Rails.env.development?
   ActionController::Base.class_eval do
     append_before_filter { Jammit.reload! }
   end
