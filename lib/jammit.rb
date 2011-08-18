33209588b13ed9d6a096bb83a1eb85b49cf5f51e
dblock <dblock@dblock.org>
2011-08-18 19:47:16 -0400

    Bad merge.

--- lib/jammit.rb
+++ lib/jammit.rb
@@ -72,8 +72,7 @@ module Jammit
     conf = YAML.load(ERB.new(File.read(config_path)).result)
 
     # Optionally overwrite configuration based on the environment.
-    rails_env = defined?(Rails) ? Rails.env : ENV['RAILS_ENV']
-    conf.merge! conf.delete rails_env if conf.has_key? rails_env
+    conf.merge! conf.delete RAILS_ENV if conf.has_key? RAILS_ENV
 
     @config_path            = config_path
     @configuration          = symbolize_keys(conf)
