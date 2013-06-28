461ec6f720b5f767ab0b362ab7dd2836ade4679d
Mariusz Michalak <stricte@gumed.edu.pl>
2013-06-28 23:34:18 +0200

    Routes fix for new version of Rails (4.x.x)

--- rails/routes.rb
+++ rails/routes.rb
@@ -1,4 +1,6 @@
 if defined?(Rails::Application)
+  major = Rails.version.split(".").first.to_i
+
   # Rails3 routes
   Rails.application.routes.draw do
     match "/#{Jammit.package_path}/:package.:extension",
@@ -6,5 +8,14 @@ if defined?(Rails::Application)
         # A hack to allow extension to include "."
         :extension => /.+/
       }
-  end
+  end if major == 3
+
+  # Rails4 routes
+  Rails.application.routes.draw do
+    get "/#{Jammit.package_path}/:package.:extension",
+      :to => 'jammit#package', :as => :jammit, :constraints => {
+        # A hack to allow extension to include "."
+        :extension => /.+/
+      }
+  end if major == 4
 end
