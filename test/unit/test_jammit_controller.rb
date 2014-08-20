feb999db9502be698d9016e9c50177ec670cce5c
Nathan Stitt <nathan@stitt.org>
2014-08-20 18:18:20 -0500

    Test jammit controller under Rails 4

--- test/unit/test_jammit_controller.rb
+++ test/unit/test_jammit_controller.rb
@@ -6,6 +6,7 @@ require 'action_controller/base'
 require 'action_controller/test_case'
 require 'jammit/controller'
 require 'jammit/routes'
+require 'action_dispatch'
 
 class JammitController
   def self.controller_path
@@ -15,31 +16,27 @@ end
 
 class JammitControllerTest < ActionController::TestCase
 
-  CACHE_DIR = '/tmp/jammit_controller_test'
-
   def setup
-    FileUtils.mkdir_p CACHE_DIR
-    ActionController::Base.page_cache_directory = CACHE_DIR
-    ActionController::Routing::Routes.draw do |map|
-      Jammit::Routes.draw(map)
+    # Perform the routing setup that Rails needs to test the controller
+    @routes = ::ActionDispatch::Routing::RouteSet.new
+    @routes.draw do
+      get "/package/:package.:extension",
+            :to => 'jammit#package', :as => :jammit, :constraints => {
+              :extension => /.+/
+          }
     end
-    Jammit.load_configuration('test/config/assets.yml').reload!
-  end
-
-  def teardown
-    FileUtils.remove_entry_secure CACHE_DIR
   end
 
   def test_package_with_jst
     get(:package, :package => 'jst_test', :extension => 'jst')
-    assert @response.headers['Content-Type'] =~ /text\/javascript/
-    assert @response.body == File.read("#{ASSET_ROOT}/fixtures/jammed/jst_test.js")
+    assert_match( /text\/javascript/, @response.headers['Content-Type'] )
+    assert_equal( File.read("#{ASSET_ROOT}/fixtures/jammed/jst_test.js"), @response.body )
   end
 
   def test_package_with_jst_mixed
     get(:package, :package => 'js_test_with_templates', :extension => 'jst')
-    assert @response.headers['Content-Type'] =~ /text\/javascript/
-    assert @response.body == File.read("#{ASSET_ROOT}/fixtures/jammed/jst_test.js")
+    assert_match( /text\/javascript/, @response.headers['Content-Type'] )
+    assert_equal( File.read("#{ASSET_ROOT}/fixtures/jammed/jst_test.js"), @response.body )
   end
 
 end
