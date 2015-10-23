502c5dfbda3ef47c9a5b0b90b076a3ec87d11674
Justin Reese <justin@justinreese.com>
2015-10-23 15:26:01 -0500

    Update docs

--- doc/js/full_list.js
+++ doc/js/full_list.js
@@ -1,11 +1,48 @@
+var inSearch = null;
+var searchIndex = 0;
+var searchCache = [];
+var searchString = '';
+var regexSearchString = '';
+var caseSensitiveMatch = false;
+var ignoreKeyCodeMin = 8;
+var ignoreKeyCodeMax = 46;
+var commandKey = 91;
+
+RegExp.escape = function(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
 function fullListSearch() {
-  $('#search input').keyup(function() {
-    var value = this.value.toLowerCase();
-    if (value == "") {
-      $('#full_list').removeClass('insearch');
-      $('#full_list li').each(function() {
-        var link = $(this).children('a:last');
-        link.text(link.text()); 
+  // generate cache
+  searchCache = [];
+  $('#full_list li').each(function() {
+    var link = $(this).find('.object_link a');
+    if (link.length === 0) return;
+    var fullName = link.attr('title').split(' ')[0];
+    searchCache.push({name:link.text(), fullName:fullName, node:$(this), link:link});
+  });
+
+  $('#search input').keyup(function(event) {
+    if ((event.keyCode > ignoreKeyCodeMin && event.keyCode < ignoreKeyCodeMax)
+         || event.keyCode == commandKey)
+      return;
+    searchString = this.value;
+    caseSensitiveMatch = searchString.match(/[A-Z]/) != null;
+    regexSearchString = RegExp.escape(searchString);
+    if (caseSensitiveMatch) {
+      regexSearchString += "|" +
+        $.map(searchString.split(''), function(e) { return RegExp.escape(e); }).
+        join('.+?');
+    }
+    if (searchString === "") {
+      clearTimeout(inSearch);
+      inSearch = null;
+      $('ul .search_uncollapsed').removeClass('search_uncollapsed');
+      $('#full_list, #content').removeClass('insearch');
+      $('#full_list li').removeClass('found').each(function() {
+
+        var link = $(this).find('.object_link a');
+        if (link.length > 0) link.text(link.text());
       });
       if (clicked) {
         clicked.parents('ul').each(function() {
@@ -15,34 +52,59 @@ function fullListSearch() {
       highlight();
     }
     else {
-      $('#full_list').addClass('insearch');
-      $('#full_list li').each(function() {
-        var link = $(this).children('a:last');
-        var text = link.text();
-        if (text.toLowerCase().indexOf(value) == -1) {
-          $(this).removeClass('found');
-          link.text(link.text());
-        }
-        else {
-          $(this).css('padding-left', '10px').addClass('found');
-          link.html(link.text().replace(new RegExp("(" + 
-            value.replace(/([\/.*+?|()\[\]{}\\])/g, "\\$1") + ")", "ig"), 
-            '<strong>$1</strong>'));
-        }
-      });
-      highlight(true);
-    }
-    
-    if ($('#full_list li:visible').size() == 0) {
-      $('#noresults').fadeIn();
-    }
-    else {
-      $('#noresults').hide();
+      if (inSearch) clearTimeout(inSearch);
+      searchIndex = 0;
+      lastRowClass = '';
+      $('#full_list, #content').addClass('insearch');
+      $('#noresults').text('');
+      searchItem();
     }
   });
-  
+
   $('#search input').focus();
-  $('#full_list').after("<div id='noresults'>No results were found.</div>")
+  $('#full_list').after("<div id='noresults'></div>");
+}
+
+var lastRowClass = '';
+function searchItem() {
+  for (var i = 0; i < searchCache.length / 50; i++) {
+    var item = searchCache[searchIndex];
+    var searchName = (searchString.indexOf('::') != -1 ? item.fullName : item.name);
+    var matchString = regexSearchString;
+    var matchRegexp = new RegExp(matchString, caseSensitiveMatch ? "" : "i");
+    if (searchName.match(matchRegexp) == null) {
+      item.node.removeClass('found');
+    }
+    else {
+      item.node.css('padding-left', '10px').addClass('found');
+      item.node.parents().addClass('search_uncollapsed');
+      item.node.removeClass(lastRowClass).addClass(lastRowClass == 'r1' ? 'r2' : 'r1');
+      lastRowClass = item.node.hasClass('r1') ? 'r1' : 'r2';
+      item.link.html(item.name.replace(matchRegexp, "<strong>$&</strong>"));
+    }
+
+    if (searchCache.length === searchIndex + 1) {
+      searchDone();
+      return;
+    }
+    else {
+      searchIndex++;
+    }
+  }
+  inSearch = setTimeout('searchItem()', 0);
+}
+
+function searchDone() {
+  highlight(true);
+  if ($('#full_list li:visible').size() === 0) {
+    $('#noresults').text('No results were found.').hide().fadeIn();
+  }
+  else {
+    $('#noresults').text('');
+  }
+  $('#content').removeClass('insearch');
+  clearTimeout(inSearch);
+  inSearch = null;
 }
 
 clicked = null;
@@ -50,6 +112,10 @@ function linkList() {
   $('#full_list li, #full_list li a:last').click(function(evt) {
     if ($(this).hasClass('toggle')) return true;
     if (this.tagName.toLowerCase() == "li") {
+      if ($(this).find('.object_link a').length === 0) {
+        $(this).children('a.toggle').click();
+        return false;
+      }
       var toggle = $(this).children('a.toggle');
       if (toggle.size() > 0 && evt.pageX < toggle.offset().left) {
         toggle.click();
@@ -57,14 +123,12 @@ function linkList() {
       }
     }
     if (clicked) clicked.removeClass('clicked');
-    var win = window.parent;
-    if (window.top.frames.main) {
-      win = window.top.frames.main;
-      var title = $('html head title', win.document).text();
-      $('html head title', window.parent.document).text(title);
-    }
+    var win;
+    try {
+      win = window.top.frames.main ? window.top.frames.main : window.parent;
+    } catch (e) { win = window.parent; }
     if (this.tagName.toLowerCase() == "a") {
-      clicked = $(this).parent('li').addClass('clicked');
+      clicked = $(this).parents('li').addClass('clicked');
       win.location = this.href;
     }
     else {
@@ -77,10 +141,10 @@ function linkList() {
 
 function collapse() {
   if (!$('#full_list').hasClass('class')) return;
-  $('#full_list.class a.toggle').click(function() { 
+  $('#full_list.class a.toggle').click(function() {
     $(this).parent().toggleClass('collapsed').next().toggleClass('collapsed');
     highlight();
-    return false; 
+    return false;
   });
   $('#full_list.class ul').each(function() {
     $(this).addClass('collapsed').prev().addClass('collapsed');
@@ -105,7 +169,7 @@ function escapeShortcut() {
   $(document).keydown(function(evt) {
     if (evt.which == 27) {
       $('#search_frame', window.top.document).slideUp(100);
-      $('#search a', window.top.document).removeClass('active inactive')
+      $('#search a', window.top.document).removeClass('active inactive');
       $(window.top).focus();
     }
   });
